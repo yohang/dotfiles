b6b965b22f57c2d728eb7850cbb57fa7a861aa69
Yohan Giarelli <yohan@les-tilleuls.coop>
2025-07-08 00:07:47 +0200

    feat: Added Python

---
+++ dotfiles/dockerized/python.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env zsh
+
+SUPPORTED_PYTHON_VERSIONS=(3.13)
+
+PYTHON_SCRIPT_DIRECTORY="${0:a:h}"
+PYTHON_IMAGE_PREFIX="ygiarelli/python"
+
+function docker_python {
+  PYTHON_IMAGE_NAME="${PYTHON_IMAGE_PREFIX}:${1}"
+
+  if [ -z "$(docker image list -q "${PYTHON_IMAGE_NAME}" 2> /dev/null)" ]; then
+     docker build \
+         --build-arg PYTHON_VERSION="${1}" \
+         --build-arg USER_ID=${UID} \
+         --build-arg USER_NAME="${USER}" \
+         -f "${PYTHON_SCRIPT_DIRECTORY}/python.Dockerfile" \
+         -t "${PYTHON_IMAGE_NAME}" \
+         "${PYTHON_SCRIPT_DIRECTORY}"
+  fi
+
+  if [ $? -ne 0 ]; then
+    echo "Failed to build Docker image ${PYTHON_IMAGE_NAME}"
+
+    return 1
+  fi
+
+  EXTRA_ARGS=()
+  if [[ "${PWD}" =~ ^"/home/" ]]; then
+      EXTRA_ARGS=(${EXTRA_ARGS} -w "${PWD}")
+  fi
+
+  docker run -it --rm \
+    -v /home:/home \
+    --network host \
+    ${EXTRA_ARGS} \
+    "${PYTHON_IMAGE_NAME}" \
+    "${@[@]:2}"
+}
+
+clean_python_docker_images() {
+  docker image list -q "${PYTHON_IMAGE_PREFIX}:*" | xargs -r docker image rm -f
+}
+
+for version in "${SUPPORTED_PYTHON_VERSIONS[@]}"; do
+  alias "python${version}"="docker_python ${version}"
+  alias python="python${version}"
+done
+
+alias pip="python pip"
+alias httpie="python httpie"
---
+++ install-deps.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env zsh
+
+sudo apt update
+sudo apt install -y \
+    build-essential \
+    btop \
+    ca-certificates \
+    cmake \
+    curl \
+    htop \
+    jq \
+    gpg \
+    vim \
+    wget
+
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+
+# Docker install
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
