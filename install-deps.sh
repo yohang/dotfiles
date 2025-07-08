c9812648cf4599486f1f4a4b22365524209705e6
Yohan Giarelli <yohan@les-tilleuls.coop>
2025-07-08 09:15:16 +0200

    feat: Added vimrc

--- install-deps.sh
+++ install-deps.sh
@@ -25,3 +25,8 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
+
+# Vim config
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_basic_vimrc.sh
+
