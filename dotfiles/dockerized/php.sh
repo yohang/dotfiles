b0d2f7906751a81518d36ac24cf2f260b617c88c
Yohan Giarelli <yohan@les-tilleuls.coop>
2025-11-21 10:07:45 +0100

    feat: Added PHP8.5

--- dotfiles/dockerized/php.sh
+++ dotfiles/dockerized/php.sh
@@ -6,7 +6,7 @@ fi
 
 source ${0:a:h}/functions.sh
 
-SUPPORTED_PHP_VERSIONS=(7.4 8.0 8.1 8.2 8.3 8.4)
+SUPPORTED_PHP_VERSIONS=(7.4 8.0 8.1 8.2 8.3 8.5 8.4)
 
 for version in "${SUPPORTED_PHP_VERSIONS[@]}"; do
   alias "php${version}"="dockerized_build_and_run php ${version}"
